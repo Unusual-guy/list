9d5ee1f67d40e5d6d0d08f23190c95ddbf0f329f
Unusual-guy <81540865+Unusual-guy@users.noreply.github.com>
2021-05-17 22:19:16 +0700

    list.h

---
+++ list.h
@@ -0,0 +1,25 @@
+#ifndef __LIST__          
+#define __LIST__          
+
+typedef struct TListItem { // имя структуры - TListItem 
+	void* data; //здесь и ниже набор переменных 
+	void* next;              
+} TListItem; 
+
+typedef void TFunc(void*); // типу void назначается имя TFunc 
+
+TListItem* list_create();
+
+TListItem* list_append(TListItem* head, void* data);
+
+TListItem* list_insert(TListItem* item, void* data);
+
+void list_traverse(TListItem* head, TFunc f);
+
+void list_free(TListItem** head);
+
+TListItem* list_search(TListItem* head, int needle);
+
+void* list_delete(TListItem* head, TListItem* item);
+
+#endif 
